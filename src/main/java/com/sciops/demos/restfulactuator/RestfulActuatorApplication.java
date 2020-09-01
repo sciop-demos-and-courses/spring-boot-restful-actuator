d7c1d111468c10714e142c119b240e64448e6206
sciops <sciops@work-P6689-MD61021>
2020-09-02 01:10:11 +0200

    initial commit

---
+++ src/main/java/com/sciops/demos/restfulactuator/RestfulActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.sciops.demos.restfulactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sciops/demos/restfulactuator/RestfulActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sciops.demos.restfulactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
